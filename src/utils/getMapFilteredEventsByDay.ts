58f8471b03374dee50390987bb71400174ed73c7
OJ Abba <phavorabba@gmail.com>
2025-03-26 17:12:08 +0000

    [Bug] Add `date` fallback when accessing `event.start.dateTime`

--- src/utils/getMapFilteredEventsByDay.ts
+++ src/utils/getMapFilteredEventsByDay.ts
@@ -1,19 +1,24 @@
-import isEmpty from "lodash/isEmpty";
-import startOfDay from "date-fns/startOfDay";
 import { getFilteredDaysFromEvents } from "./getFilteredDaysFromEvents";
+import startOfDay from "date-fns/startOfDay";
 import type { EventType, DateTime } from "../types";
 
 const getMapFilteredEventsByDay = (
   events?: EventType[],
 ): Record<DateTime, EventType[]> => {
-  if (!Array.isArray(events) || isEmpty(events)) {
+
+  if (!Array.isArray(events) || events.length <= 0) {
     return {};
   }
-
   const daysList = getFilteredDaysFromEvents(events);
 
   return daysList.reduce<Record<DateTime, EventType[]>>((acc, day) => {
-    acc[day] = events.filter((event) => startOfDay(new Date(event.start.dateTime)).toISOString() === day);
+
+    acc[day] = events.filter((event) => {
+
+      const dateTime = event.start.dateTime ?? event.start.date
+
+      return startOfDay(new Date(dateTime)).toISOString() === day
+    });
     // sort by date
     acc[day].sort((a, b) => new Date(a.start.dateTime).getTime() - new Date(b.start.dateTime).getTime());
     return acc;
